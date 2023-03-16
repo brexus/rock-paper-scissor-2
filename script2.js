2659355e6b4c653b4a88a305e24cdae96f5d894c
brexus <92671269+brexus@users.noreply.github.com>
2023-03-16 12:12:04 +0100

    fix margins and change page container structure

--- script2.js
+++ script2.js
@@ -5,6 +5,8 @@ function getPlayerChoice() {
     const rps_buttons = document.querySelectorAll('[data-element-id="1"]');
     let compChoice;
 
+        // document.querySelector("finish-window").classList.remove("active");
+        // document.querySelector("main").classList.remove("low-opacity");
     for (let i = 0; i < 3; i++) {
         rps_buttons[i].addEventListener('click', function(e) {
             for (let j = 0; j < 3; j++) {
@@ -17,42 +19,53 @@ function getPlayerChoice() {
             playRound(i, compChoice);
         });
     }
+
+    // document.getElementsByClassName("finish-window").classList.add("active");
+    // document.getElementsByClassName("main").classList.add("low-opacity");
 }
+
+
 
 function getComputerChoice() {
     let compChoice = Math.floor(Math.random() * 3);
     return compChoice;
 }
 
+
 function playRound(playerChoice, compChoice) {
+    if (playerScore < 5 && compScore < 5){
+        if(playerChoice == compChoice ){
+            console.log("You draw!");
+        } else if (playerChoice == 0 && compChoice == 1) {
+            compScore++;
+            console.log("You lose! Paper beats rock");
+        } else if (playerChoice == 0 && compChoice == 2) {
+            playerScore++;
+            console.log("You win! Rock beats scissor");
+        } else if (playerChoice == 1 && compChoice == 0) {
+            playerScore++;
+            console.log("You win! Paper beats rock");
+        } else if (playerChoice == 1 && compChoice == 2) {
+            compScore++;
+            console.log("You lose! Scissor beats paper");
+        } else if (playerChoice == 2 && compChoice == 0) {
+            compScore++;
+            console.log("You lose! Rock beats scissor");
+        } else if (playerChoice == 2 && compChoice == 1) {
+            playerScore++;
+            console.log("You win! Scissor beats paper");
+        } else console.log("error");
 
-    if(playerChoice == compChoice ){
-        playerScore++;
-        compScore++;
-        console.log("You draw!");
-    } else if (playerChoice == 0 && compChoice == 1) {
-        compScore++;
-        console.log("You lose! Paper beats rock");
-    } else if (playerChoice == 0 && compChoice == 2) {
-        playerScore++;
-        console.log("You win! Rock beats scissor");
-    } else if (playerChoice == 1 && compChoice == 0) {
-        playerScore++;
-        console.log("You win! Paper beats rock");
-    } else if (playerChoice == 1 && compChoice == 2) {
-        compScore++;
-        console.log("You lose! Scissor beats paper");
-    } else if (playerChoice == 2 && compChoice == 0) {
-        compScore++;
-        console.log("You lose! Rock beats scissor");
-    } else if (playerChoice == 2 && compChoice == 1) {
-        playerScore++;
-        console.log("You win! Scissor beats paper");
-    } else console.log("error");
+        updateScore(playerChoice, compChoice);
+        updateDuelImg(playerChoice, compChoice);
+        updateHistory(playerChoice, compChoice);
 
-    updateScore(playerChoice, compChoice);
-    updateDuelImg(playerChoice, compChoice);
-    updateHistory(playerChoice, compChoice);
+    } else {
+        // document.querySelector("finish-window").classList.add('active');
+        // document.querySelector("main").classList.add('low-opacity');
+    }
+
+
 }
 
 function updateScore(playerChoice, compChoice) {
@@ -105,3 +118,4 @@ function updateHistory(playerChoice, compChoice) {
     history_comp_element.appendChild(history_comp_text);
     document.querySelector('[data-history="2"]').appendChild(history_comp_element);
 }
+
